16315e8b7fd29856b8bd3204a5f786243632f386
khforever <59407743+khforever@users.noreply.github.com>
2026-01-08 06:38:18 +0200

    Four Decorating Imges

--- src/components/DecoratingImg/index.jsx
+++ src/components/DecoratingImg/index.jsx
@@ -7,28 +7,28 @@ const DecoratingImg = () => {
   return (
     <section className="relative min-h-screen bg-gray-100 overflow-visible">
       
-      {/* الصورة العلوية اليسار */}
+   
       <img
         src={img1}
         alt="Decorative Image 1"
         className="absolute top-10 left-20  translate-x-16  w-[380px] z-10"
       />
 
-      {/* الصورة الكبيرة يمين */}
+   
       <img
         src={img2}
         alt="Decorative Image 2"
         className=" absolute  top-20 right-20   -translate-x-12  w-[320px] md:w-[500px] lg:w-[600px] z-20"
       />
 
-      {/* الصورة اللي في النص */}
+      
       <img
         src={img3}
         alt="Decorative Image 3"
         className="absolute top-[350px] left-1/2 -translate-x-1/2 w-[420px] z-30"
       />
 
-      {/* الصورة السفلية يمين */}
+      
       <img
         src={img4}
         alt=" Decorative Image 4"
